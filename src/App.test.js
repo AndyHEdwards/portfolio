61c910cac31be4b8003fcda29070d4baba5ec122
Andy Edwards <andyhedwards@gmail.com>
2018-05-14 13:42:38 +0100

    Add work component

--- src/App.test.js
+++ src/App.test.js
@@ -2,10 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import NavBar from './components/NavBar/NavBar';
 import Hero from './components/Hero/Hero';
+import Work from './components/Work/Work';
 
 it('renders the nav bar and Hero without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<NavBar />, div);
   ReactDOM.render(<Hero />, div);
+  ReactDOM.render(<Work />, div);
   ReactDOM.unmountComponentAtNode(div);
 });
---
+++ src/components/Work/Work.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import styles from './Work.module.scss';
+
+const Work = () => {
+  return (
+    <section className={styles.Work}>
+      <h2 className={styles.heading}>Work</h2>
+    </section>
+  );
+}
+
+export default Work;
