77d2125a44c9ccc523dc86ed93fa4469527335b1
Andy Edwards <andyhedwards@gmail.com>
2018-05-14 13:27:34 +0100

    Redefine heading and subheading styles

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -4,8 +4,8 @@ import styles from './Hero.module.scss';
 const Hero = () => {
   return (
     <section className={styles.Hero}>
-      <div className={styles.titleWrapper}>
-        <h1 className={styles.title}>
+      <div className={styles.headingWrapper}>
+        <h1 className={styles.heading}>
           Freelance Strategist, Designer and developer who specialises in working remotely.
         </h1>
       </div>
--- src/components/Services/Services.js
+++ src/components/Services/Services.js
@@ -7,22 +7,22 @@ import iconLaptop from '../../assets/icons/icon-laptop.svg';
 const Services = () => {
   return (
     <section className={styles.ServicesSection}>
-      <h2 className={styles.title}>Services</h2>
+      <h2 className={styles.heading}>Services</h2>
 
       <div className={styles.services}>
         <div className={styles.service}>
           <img className={styles.icon} src={iconStrategy} alt='icon-Strategy' />
-          <div className={styles.subTitle}>Strategy</div>
+          <div className={styles.subHeading}>Strategy</div>
           <div className={styles.content}>From inception to creatoin I can help make your dream product become a reality all the way through the build process</div>
         </div>
         <div className={styles.service}>
           <img className={styles.icon} src={lightBulb} alt='icon-lightBulb' />
-          <div className={styles.subTitle}>Design</div>
+          <div className={styles.subHeading}>Design</div>
           <div className={styles.content}>With my up to date knowloedge of the lateset and greatest design trends, I blend seamless functionality with cutting edge design.</div>
         </div>
         <div className={styles.service}>
           <img className={styles.icon} src={iconLaptop} alt='icon-Laptop' />
-          <div className={styles.subTitle}>Development</div>
+          <div className={styles.subHeading}>Development</div>
           <div className={styles.content}>With many years of development experience under my belt, I can build your product.</div>
         </div>
       </div>
