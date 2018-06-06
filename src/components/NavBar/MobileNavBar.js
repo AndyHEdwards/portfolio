09114615a905e3971ce99c0544d95f8fa9ce051e
Andy Edwards <andyhedwards@gmail.com>
2018-06-06 13:19:24 +0100

    Add mobile hamburger menu

---
+++ src/components/NavBar/MobileNavBar.js
@@ -0,0 +1,47 @@
+export default  {
+  bmBurgerButton: {
+    position: 'absolute',
+    width: '26px',
+    height: '23px',
+    right: '20px',
+    top: '23px'
+  },
+  bmBurgerBars: {
+    background: '#373a47',
+    height: '12%'
+  },
+  bmCrossButton: {
+    height: '24px',
+    width: '24px'
+  },
+  bmCross: {
+    background: '#bdc3c7',
+    height: '30px',
+  },
+  bmMenuWrap: {
+    top: 0,
+    width: '100%',
+  },
+  bmMenu: {
+    background: '#373a47',
+    width: '100%',
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  bmMorphShape: {
+    fill: '#373a47'
+  },
+  bmItemList: {
+    color: '#b8b7ad',
+    padding: '0.8em',
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'space-between',
+    maxHeight: '365px',
+    textAlign: 'center',
+  },
+  bmOverlay: {
+    background: 'rgba(0, 0, 0, 0.3)'
+  }
+}
--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,31 +1,61 @@
 import React from 'react';
 import styles from './NavBar.module.scss';
+import mobileNavBarStyles from './MobileNavBar.js';
+
+import { slide as Menu } from 'react-burger-menu'
 
 import AnchorLink from 'react-anchor-link-smooth-scroll'
 
-const NavBar = () => {
-  return (
-    <div className={styles.navBar}>
-      <div className={styles.content}>
-        <div className={styles.logo}>Andy Edwards</div>
+export default class NavBar extends React.Component {
 
-        <div className={styles.links}>
-          <div className={styles.wrapper}>
-            <AnchorLink className={styles.link} href='#hero'>Home</AnchorLink>
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      isMobileNavOpen: false
+    };
+
+    this.handleMobileNavClick = this.handleMobileNavClick.bind(this);
+  }
+
+  handleMobileNavClick() {
+    this.setState({
+      isMobileNavOpen: false
+    });
+  }
+
+  render() {
+
+    return (
+      <div className={styles.navBar}>
+        <div className={styles.content}>
+          <div className={styles.logo}>Andy Edwards</div>
+
+          <div className={styles.links}>
+            <div className={styles.wrapper}>
+              <AnchorLink className={styles.link} href='#hero'>Home</AnchorLink>
+            </div>
+            <div className={styles.wrapper}>
+              <AnchorLink className={styles.link} href='#services'>Services</AnchorLink>
+            </div>
+            <div className={styles.wrapper}>
+              <AnchorLink className={styles.link} href='#work'>Work</AnchorLink>
+            </div>
+            <div className={styles.wrapper}>
+              <AnchorLink className={styles.link} href='#contact'>Contact</AnchorLink>
+            </div>
           </div>
-          <div className={styles.wrapper}>
-            <AnchorLink className={styles.link} href='#services'>Services</AnchorLink>
-          </div>
-          <div className={styles.wrapper}>
-            <AnchorLink className={styles.link} href='#work'>Work</AnchorLink>
-          </div>
-          <div className={styles.wrapper}>
-            <AnchorLink className={styles.link} href='#contact'>Contact</AnchorLink>
+
+          <div className={styles.mobileNav}>
+            <Menu right styles={mobileNavBarStyles} isOpen={this.state.isMobileNavOpen}>
+              <AnchorLink onClick={ this.handleMobileNavClick } className={styles.mobileLink} href='#hero'>Home</AnchorLink>
+              <AnchorLink onClick={ this.handleMobileNavClick } className={styles.mobileLink} href='#services'>Services</AnchorLink>
+              <AnchorLink onClick={ this.handleMobileNavClick } className={styles.mobileLink} href='#work'>Work</AnchorLink>
+              <AnchorLink onClick={ this.handleMobileNavClick } className={styles.mobileLink} href='#contact'>Contact</AnchorLink>
+            </Menu>
           </div>
         </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
-
-export default NavBar;
